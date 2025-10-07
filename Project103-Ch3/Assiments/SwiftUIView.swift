cf1760a9d812ca3ccd6928a5ceeebbd249017f8a
nrasaily <nareshsathi79@gmail.com>
2025-10-06 20:10:15 -0400

    assignment 1 completed

--- Project103-Ch3/Assiments/SwiftUIView.swift
+++ Project103-Ch3/Assiments/SwiftUIView.swift
@@ -8,10 +8,50 @@
 import SwiftUI
 
 struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    let student = ["Nar", "Hari", "Harka", "Sapana", "Arjun"]
+    
+    let studentGrades = [
+        "Nar": [88, 90, 97, 88, 93],
+        "Hari": [90, 76, 88, 99, 09],
+        "Harka": [89, 80, 98, 99, 78],
+        "Sapana": [76, 98, 70, 48, 88],
+        "Arjun": [99, 88, 77, 90, 88]
+    ]
+    
+    
+    // 🧮 Function to calculate total grades for a student
+    func calculateAverage(_ studentGrades: [String: [Int]]) -> Double {
+        var total = 0
+        var count = 0
+        
+        for(name, grades) in studentGrades{
+            for grade in grades {
+                total += grade
+                count += 1
+            }
+        }
+        return Double(total) / Double(count)
     }
-}
+    
+    var body: some View {
+        let average = calculateAverage(studentGrades)
+        
+        VStack {
+            
+            Text("Students")
+            
+            Text("AVG:  \(average)")
+            
+            List(students, id: \.self) { student in
+                Text("Name \(student)")
+             
+            }
+            .listStyle(GroupedListStyle())
+        }
+        
+        }
+    }
+
 
 #Preview {
     SwiftUIView()
--- Project103-Ch3/Assiments/assignment 1.swift
+++ Project103-Ch3/Assiments/assignment 1.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+let students = ["Hari", "Puja", "Ravi", "Jagadish", "Harka"]
+
+//print("students \(students)")
+
+let studentgrades: [String: [Int]] = [
+    "Hari": [88, 92, 79, 95, 84],
+    "Puja": [76, 97, 54, 99, 100],
+    "Ravi": [89, 91, 78, 94, 85],
+    "Jagadish": [74, 86, 52, 98, 90],
+    "Harka": [81, 93, 77, 96, 83]
+]
+
